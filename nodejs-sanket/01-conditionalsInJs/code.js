15b22e26c1683699a16dd6b83f5a6b38d894c7cf
Moulali Syed <syedmoulali1997@gmail.com>
2023-01-30 22:18:00 +0530

    conditionals in JS

---
+++ nodejs-sanket/01-conditionalsInJs/code.js
@@ -0,0 +1,69 @@
+// let day = 'mng';
+// if (day === 'mng') {
+//   console.log('hi');
+// } else {
+//   console.log('bye');
+// }
+
+// // -----
+
+// let subType = 'premium';
+// let vipMem = true;
+
+// if (subType === 'premium') {
+//   console.log('hi');
+// } else if (vipMem) {
+//   console.log('hello');
+// } else {
+//   console.log('bye');
+// }
+
+// let num = 27;
+
+// if (num % 2 === 0) {
+//   console.log('even');
+// } else {
+//   console.log('odd');
+// }
+
+// let res = num % 2 === 0 ? 'even' : 'odd';
+
+// let num1 = 15;
+// let num2 = 20;
+// let num3 = 20;
+
+// if (num1 >= num2) {
+//   if (num1 >= num3) {
+//     console.log(num1, 'is  big');
+//   }
+// } else if (num2 >= num3) {
+//   console.log(num2, 'is big');
+// } else {
+//   console.log(num3, 'is big');
+// }
+
+// if (15 > 17) {
+//   console.log('hi');
+// }
+
+// let a = 2;
+// let b = 3;
+// let c = 4;
+
+// if (a + b >= c || a + c >= b || b + c >= a) {
+//   console.log('triangle can be formed');
+// } else {
+//   console.log('triangle cannot be formed');
+// }
+
+let a = 7;
+let b = 7;
+let c = 5;
+
+if ((a == b) == c) {
+  console.log('equlateral triangle');
+} else if (a === b || a === c || b === c) {
+  console.log('scalene triangle');
+} else {
+  console.log('isoceles triangle');
+}
