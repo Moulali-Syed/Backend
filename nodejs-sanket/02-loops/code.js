104afda9b20f51504eedef7fb034ca21091d5c86
Moulali Syed <syedmoulali1997@gmail.com>
2023-01-30 22:32:05 +0530

    loops

---
+++ nodejs-sanket/02-loops/code.js
@@ -0,0 +1,32 @@
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// //while loop
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   sum += i;
+// }
+
+// let sum1 = 0;
+// let j = 1;
+// while (j <= 10) {
+//   sum1 += j;
+//   j++;
+// }
+
+// console.log(sum, sum1);
+
+// let i = 20;
+
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
