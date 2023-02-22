f141a8d083cd15435149ce15e026b97aac4634d0
Moulali Syed <syedmoulali1997@gmail.com>
2023-02-22 12:45:05 +0530

    nr session 3

--- ReactJs-namasteDev/react/lec2/App.js
+++ ReactJs-namasteDev/react/lec2/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading1 = React.createElement('h1', {}, 'Namaste React from parcel');
+const heading1 = React.createElement(
+  'h1',
+  { key: 'h1' },
+  'Namaste React from parcel'
+);
 const heading2 = React.createElement(
   'h2',
-  {},
+  { key: 'h2' },
   'Hello Welcome to Frontend framework'
 );
 const container = React.createElement('div', {}, [heading1, heading2]);
 
+/*
+we have a requirement as below 
+*/
+
+const h1 = React.createElement('h1', {}, 'Namaste React');
+const li1 = React.createElement('li', {}, 'About Us');
+const li2 = React.createElement('li', {}, 'Support');
+const li3 = React.createElement('li', {}, 'Help');
+const ul = React.createElement('ul', {}, [li1, li2, li3]);
+const heading = React.createElement('div', { className: 'header' }, [h1, ul]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(container);
+// root.render(container);
+root.render(heading);
---
+++ Wesbos-30JSProjects/01-JavascriptDrumKit/script.js
@@ -0,0 +1,10 @@
+// const elems = document.querySelectorAll('.b');
+// elems.forEach((ele) => {
+//   ele.addEventListener('keypress', (e) => {
+//     console.log(e);
+//   });
+// });
+
+window.addEventListener('keypress', (e) => {
+  console.log(e);
+});
